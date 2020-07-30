27604a3a5e29fc1aed7dcdc543c6c1d9502b2b87
Jayashri <jayashri1394@gmail.com>
2020-07-31 01:29:24 +0200

    npm package is built

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,8 +3,7 @@ import { body,validationResult } from 'express-validator';
 import jwt from 'jsonwebtoken';
 
 import { User } from '../models/user';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest,BadRequestError } from '@jaytickets/common';
 
 const router = express.Router();
 
--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -27,4 +27,3 @@ const start = async () => {
 };
 
 start();
-
