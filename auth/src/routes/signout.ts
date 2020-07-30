e46c93d51664a5f5061d14e5c4a37d44771dff9e
Jayashri <jayashri1394@gmail.com>
2020-07-30 19:51:21 +0200

    auth service is completed

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.post('/api/users/signout', (req, res) => {
-  //req.session = null;
+  req.session = null;
 
   res.send({});
 });
