7c7d430e4b42795b8560fc41b0e03c0bff3078d0
Jayashri <jayashri1394@gmail.com>
2020-07-31 20:08:12 +0200

    nats streaming server is created

---
+++ nats-test/src/publisher.ts
@@ -0,0 +1,29 @@
+import nats from 'node-nats-streaming';
+
+console.clear();
+
+const stan = nats.connect('ticketapp', 'abc', {
+  url: 'http://localhost:4222',
+});
+
+stan.on('connect', () => {
+  console.log('Publisher connected to NATS');
+
+  stan.on('close', () => {
+    console.log('NATS connection closed!');
+    process.exit();
+  });
+
+  const data = JSON.stringify({
+    id: '123',
+    title: 'concert',
+    price: 20,
+  });
+
+  stan.publish('ticket:created', data, () => {
+    console.log('Event published');
+  });
+});
+
+process.on('SIGINT', () => stan.close());
+process.on('SIGTERM', () => stan.close());
