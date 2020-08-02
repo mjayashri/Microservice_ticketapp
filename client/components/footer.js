eccdb69adc4804810adbb0c07d059eb662f4b76b
Jayashri <jayashri1394@gmail.com>
2020-08-02 23:33:40 +0200

    Nextjs server side rendered

---
+++ client/components/footer.js
@@ -0,0 +1,9 @@
+import Link from 'next/link';
+
+export default () => {
+    return (
+        <div className="d-flex justify-content-end container">
+            Made by Jayashri
+        </div>
+    );
+};
